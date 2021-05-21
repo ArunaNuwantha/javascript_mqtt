62afe068fcd0e0ee73ea103e0164e53123e413f6
Aruna Nuwantha <e16261@eng.pdn.ac.lk>
2021-05-22 00:15:05 +0530

    delete user file & set the publisher as one file

--- app/publisher.js
+++ app/publisher.js
@@ -1,10 +1,50 @@
 const mqtt = require("mqtt");
 const mongoose = require("mongoose");
-const { User } = require("./user");
 
 const client = mqtt.connect("mqtt://test.mosquitto.org");
 const topic = "spm/device/12345";
 const device_id = topic.slice(11);
+
+const userSchema = new mongoose.Schema({
+  deviceID: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    min: 5,
+    max: 60,
+  },
+
+  patients: {
+    type: [],
+  },
+
+  containers: {
+    type: [],
+  },
+
+  history: {
+    type: [],
+  },
+
+  password: {
+    type: String,
+    required: true,
+    min: 5,
+    max: 1024,
+  },
+
+  isVerified: { type: Boolean, default: false },
+});
+
+const User = mongoose.model("user", userSchema);
 
 const uri =
   "mongodb+srv://spm-user:1234@cluster0.pps0b.mongodb.net/spmdb?retryWrites=true&w=majority";
@@ -30,8 +70,4 @@ async function publisher() {
   } catch (error) {}
 }
 
-setInterval(() => {
-  publisher();
-}, 5000);
-
-// fetchDataFromMongoDB();
+publisher();
